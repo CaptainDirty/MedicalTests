132bdbbcc273ad8f73a36cb2c3c7563a27cf569e
Bolotov.Alexander <Bolotov.Alexander@urfu.me>
2021-05-27 12:53:29 +0500

    Готов к изменению

---
+++ ErrorProject.Domain/Categories.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MedicalTests.Domain
+{
+    public class Categories
+    {
+        /// <summary>
+        /// ID категории
+        /// </summary>
+        [Key]
+        public int ID_Category { get; set; }
+
+        /// <summary>
+        /// Наименование пользователя
+        /// </summary>
+        [Required(ErrorMessage = "Вы не ввели наименование категории ошибки")]        
+        [Display(Name = "Наименование категории ошибки программного обеспечения")]
+        public string NameCategory { get; set; }
+
+        public UserProfile Owner { get; set; }
+    }
+}
